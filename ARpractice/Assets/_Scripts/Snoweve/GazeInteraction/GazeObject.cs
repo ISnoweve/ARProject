7bd80d91ebacf3a671431bdab2e289beb1d2c26b
Snoweve <a0976056641@gmail.com>
2024-11-27 23:25:02 +0800

    InfoObject and Input update

--- ARpractice/Assets/_Scripts/Snoweve/GazeInteraction/GazeObject.cs
+++ ARpractice/Assets/_Scripts/Snoweve/GazeInteraction/GazeObject.cs
@@ -3,15 +3,5 @@ using UnityEngine;
 
 public class GazeObject : MonoBehaviour
 {
-    InfoBehaviour infoBehaviour;
-
-    private void Awake()
-    {
-        infoBehaviour = GetComponentInChildren<InfoBehaviour>();
-    }
-
-    public void OnGazeEnter()
-    {
-        Debug.Log(  gameObject.name+"Gaze Enter");
-    }
+    public string gazeObjectID;
 }
